90c136d63a3bbc0081c25950752132378acd7dd5
Laidone <laidonecarvalho@hotmail.com>
2025-01-02 19:31:19 -0300

    Ajuste endpoint dinâmico

--- api/Controller/controller.py
+++ api/Controller/controller.py
@@ -1,34 +1,33 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 import Service.Scraper as scraper
 from utils.auth_util import get_current_user
+from typing import Optional
+
 
 
 router = APIRouter(
     dependencies=[Depends(get_current_user)]  # Aplica a validação JWT em todas as rotas
 )
 
+subOptProces = {"Viníferas": "01", "Americanas e híbridas": "02", "Uvas de mesa": "03" , "Sem classificação": "04"}
+subImpExp = {"Vinhos de mesa": "01", "Espumantes": "02", "Uvas frescas": "03" , "Uvas passas": "04", "Suco de uva": "05"}
 
+@router.post("/Produção", tags=["Produção"])
+async def get_producao(ano: int = Query(None, ge=1970, le=2023)):
+    return scraper.scraper_dados("02", ano)
 
-@router.get("/Produção", tags=["Produção"])
-async def get_producao():
-    scraper.scraper_dados("producao")
-    return 1
+@router.post("/Processamento", tags=["Processamento"])
+async def get_processamento(ano : int = Query(None, ge=1970, le=2023), subopt: Optional[str] = Query(None, enum=["Viníferas", "Americanas e híbridas", "Uvas de mesa", "Sem classificação"])):
+    return scraper.scraper_dados("03", ano, subOptProces.get(subopt))
 
-@router.get("/Processamento", tags=["Processamento"])
-async def get_processamento():
-    scraper.scraper_dados("processamento")
-    return 1
-@router.get("/Comercialização", tags=["Comercialização"])
-async def get_comercializacao():
-    scraper.scraper_dados("comercializacao")
-    return 1
+@router.post("/Comercialização", tags=["Comercialização"])
+async def get_comercializacao(ano : int = Query(None, ge=1970, le=2023)):
+    return scraper.scraper_dados("04", ano)
 
-@router.get("/Importação", tags=["Importação"])
-async def get_importacao():
-    scraper.scraper_dados_impo_expor("importacao")
-    return 1
+@router.post("/Importação", tags=["Importação"])
+async def get_importacao(ano : int = Query(None, ge=1970, le=2023), subopt: Optional[str] = Query(None, enum=["Vinhos de mesa", "Espumantes", "Uvas frescas", "Uvas passas", "Suco de uva"])):
+    return scraper.scraper_dados("05", ano, subImpExp.get(subopt))
 
-@router.get("/Exportação", tags=["Exportação"])
-async def get_exportacao():
-    scraper.scraper_dados_impo_expor("exportacao")
-    return 1
+@router.post("/Exportação", tags=["Exportação"])
+async def get_exportacao(ano : int = Query(None, ge=1970, le=2023), subopt: Optional[str] = Query(None, enum=["Vinhos de mesa", "Espumantes", "Uvas frescas", "Uvas passas", "Suco de uva"])):
+    return scraper.scraper_dados("06", ano, subImpExp.get(subopt))
--- api/Service/Scraper.py
+++ api/Service/Scraper.py
@@ -1,18 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+from typing import Optional
 
 #Faz o scraping dos dados de produção, processamento e comercialização do site da Embrapa.
-def scraper_dados(aba):
-    if(aba == "producao"):
-        url = "http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_02"
-    elif(aba == "processamento"):
-        url = "http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_03"
-    elif(aba == "comercializacao"):
-        url = "http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_04"
-    elif(aba == "importacao"):
-        url = "http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_05"
-    elif(aba == "exportacao"):
-        url = "hhttp://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_06"
+def scraper_dados(opcao: str, ano: Optional[str] = None ,subopt: Optional[str] = 1):
+    
+    url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao=opt_{opcao}&subopcao=subopt_{subopt}"
         
     response = requests.get(url)
     
@@ -23,38 +16,22 @@ def scraper_dados(aba):
     table = table = soup.find("table", {"class": "tb_base tb_dados"})  # Localiza a tabela desejada
     data = []
     
-    # Iterar sobre as linhas da tabela (exceto cabeçalhos)
-    for row in table.find_all("tr")[1:]:
-        cols = row.find_all("td")
-        data.append({
-            "Produto": cols[0].text.strip(),
-            "Quantidade": None if (cols[1].text.strip() == "-") else (int(cols[1].text.strip().replace(".", ""))) # Converter para número
-        })
-    
-    return data
-
-def scraper_dados_impo_expor(aba):
-    if(aba == "importacao"):
-        url = "http://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_05"
-    elif(aba == "exportacao"):
-        url = "hhttp://vitibrasil.cnpuv.embrapa.br/index.php?opcao=opt_06"
-        
-    response = requests.get(url)
-    
-    if response.status_code != 200:
-        raise Exception(f"Erro ao acessar os dados: {response.status_code}")
-    
-    soup = BeautifulSoup(response.text, "html.parser")
-    table = table = soup.find("table", {"class": "tb_base tb_dados"})  # Localiza a tabela desejada
-    data = []
-    
-    # Iterar sobre as linhas da tabela (exceto cabeçalhos)
-    for row in table.find_all("tr")[1:]:
-        cols = row.find_all("td")
-        data.append({
-            "País": cols[0].text.strip(),
-            "Quantidade": None if (cols[1].text.strip() == "-") else (int(cols[1].text.strip().replace(".", ""))),
-            "Valor": None if (cols[2].text.strip() == "-") else (int(cols[2].text.strip().replace(".", "")))
-        })
+    if(opcao == "02" or opcao == "03" or opcao == "04"):
+        # Iterar sobre as linhas da tabela (exceto cabeçalhos)
+        for row in table.find_all("tr")[1:]:
+            cols = row.find_all("td")
+            data.append({
+                "Produto": cols[0].text.strip(),
+                "Quantidade": None if (cols[1].text.strip() == "-") else (int(cols[1].text.strip().replace(".", ""))) # Converter para número
+            })
+    elif(opcao == "05" or opcao == "06"):
+        # Iterar sobre as linhas da tabela (exceto cabeçalhos)
+        for row in table.find_all("tr")[1:]:
+            cols = row.find_all("td")
+            data.append({
+                "País": cols[0].text.strip(),
+                "Quantidade": None if (cols[1].text.strip() == "-") else (int(cols[1].text.strip().replace(".", ""))),
+                "Valor": None if (cols[2].text.strip() == "-") else (int(cols[2].text.strip().replace(".", "")))
+            })
     
     return data
