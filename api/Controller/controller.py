75ae62725e261c776a8a28d341adf57cf958aa4f
Laidone <laidonecarvalho@hotmail.com>
2025-01-03 10:20:39 -0300

    Ajuste endpoint dinâmico

--- api/Controller/controller.py
+++ api/Controller/controller.py
@@ -12,22 +12,22 @@ router = APIRouter(
 subOptProces = {"Viníferas": "01", "Americanas e híbridas": "02", "Uvas de mesa": "03" , "Sem classificação": "04"}
 subImpExp = {"Vinhos de mesa": "01", "Espumantes": "02", "Uvas frescas": "03" , "Uvas passas": "04", "Suco de uva": "05"}
 
-@router.post("/Produção", tags=["Produção"])
+@router.get("/Produção", tags=["Produção"])
 async def get_producao(ano: int = Query(None, ge=1970, le=2023)):
     return scraper.scraper_dados("02", ano)
 
-@router.post("/Processamento", tags=["Processamento"])
+@router.get("/Processamento", tags=["Processamento"])
 async def get_processamento(ano : int = Query(None, ge=1970, le=2023), subopt: Optional[str] = Query(None, enum=["Viníferas", "Americanas e híbridas", "Uvas de mesa", "Sem classificação"])):
     return scraper.scraper_dados("03", ano, subOptProces.get(subopt))
 
-@router.post("/Comercialização", tags=["Comercialização"])
+@router.get("/Comercialização", tags=["Comercialização"])
 async def get_comercializacao(ano : int = Query(None, ge=1970, le=2023)):
     return scraper.scraper_dados("04", ano)
 
-@router.post("/Importação", tags=["Importação"])
+@router.get("/Importação", tags=["Importação"])
 async def get_importacao(ano : int = Query(None, ge=1970, le=2023), subopt: Optional[str] = Query(None, enum=["Vinhos de mesa", "Espumantes", "Uvas frescas", "Uvas passas", "Suco de uva"])):
     return scraper.scraper_dados("05", ano, subImpExp.get(subopt))
 
-@router.post("/Exportação", tags=["Exportação"])
+@router.get("/Exportação", tags=["Exportação"])
 async def get_exportacao(ano : int = Query(None, ge=1970, le=2023), subopt: Optional[str] = Query(None, enum=["Vinhos de mesa", "Espumantes", "Uvas frescas", "Uvas passas", "Suco de uva"])):
     return scraper.scraper_dados("06", ano, subImpExp.get(subopt))
