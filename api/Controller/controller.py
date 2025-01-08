3a20142d143e5e88edd599565302ff02ccd27dd5
Laidone <laidonecarvalho@hotmail.com>
2025-01-07 22:42:58 -0300

    Integração com banco de dados + modelos

--- api/Controller/controller.py
+++ api/Controller/controller.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, Query
-import Service.Scraper as scraper
+from Service import scraper, service
 from utils.auth_util import get_current_user
 from typing import Optional
 
@@ -18,16 +18,36 @@ async def get_producao(ano: int = Query(None, ge=1970, le=2023)):
 
 @router.get("/Processamento", tags=["Processamento"])
 async def get_processamento(ano : int = Query(None, ge=1970, le=2023), subopt: Optional[str] = Query(None, enum=["Viníferas", "Americanas e híbridas", "Uvas de mesa", "Sem classificação"])):
-    return scraper.scraper_dados("03", ano, subOptProces.get(subopt))
+    return scraper.scraper_dados("03", ano, subOptProces.get(subopt), subopt)
+
+@router.post("/Processamento", tags=["Processamento"])
+async def get_processamento(ano : int = Query(None, ge=1970, le=2023), subopt: Optional[str] = Query(None, enum=["Viníferas", "Americanas e híbridas", "Uvas de mesa", "Sem classificação"])):
+    return service.insert_dados_processamento("03", ano, subOptProces.get(subopt), subopt)
 
 @router.get("/Comercialização", tags=["Comercialização"])
 async def get_comercializacao(ano : int = Query(None, ge=1970, le=2023)):
     return scraper.scraper_dados("04", ano)
 
+@router.post("/ComercializaçãoInserir", tags=["Comercialização"])
+async def get_comercializacao(ano : int = Query(None, ge=1970, le=2023)):
+    return service.insert_dados_comercializacao("04", ano)
+
 @router.get("/Importação", tags=["Importação"])
 async def get_importacao(ano : int = Query(None, ge=1970, le=2023), subopt: Optional[str] = Query(None, enum=["Vinhos de mesa", "Espumantes", "Uvas frescas", "Uvas passas", "Suco de uva"])):
     return scraper.scraper_dados("05", ano, subImpExp.get(subopt))
 
+@router.post("/ImportaçãoInserir", tags=["Importação"])
+async def get_importacao(ano : int = Query(None, ge=1970, le=2023), subopt: Optional[str] = Query(None, enum=["Vinhos de mesa", "Espumantes", "Uvas frescas", "Uvas passas", "Suco de uva"])):
+    return service.insert_dados_importacao("05",ano, subImpExp.get(subopt), subopt)
+
 @router.get("/Exportação", tags=["Exportação"])
 async def get_exportacao(ano : int = Query(None, ge=1970, le=2023), subopt: Optional[str] = Query(None, enum=["Vinhos de mesa", "Espumantes", "Uvas frescas", "Uvas passas", "Suco de uva"])):
-    return scraper.scraper_dados("06", ano, subImpExp.get(subopt))
+    return scraper.scraper_dados("06", ano, subImpExp.get(subopt), subopt)
+
+@router.post("/ExportaçãoInserir", tags=["Exportação"])
+async def get_exportacao(ano : int = Query(None, ge=1970, le=2023), subopt: Optional[str] = Query(None, enum=["Vinhos de mesa", "Espumantes", "Uvas frescas", "Uvas passas", "Suco de uva"])):
+    return service.insert_dados_exportacao("06",ano, subImpExp.get(subopt), subopt)
+
+@router.post("/Teste", tags=["Teste"])
+async def get_exportacao():
+    return service.insert_dados("07")
--- api/utils/auth_util.py
+++ api/utils/auth_util.py
@@ -1,8 +1,12 @@
+import pandas as pd
 from datetime import datetime, timedelta
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
 from passlib.context import CryptContext
+from database.db_connector import *
+from Service import service
+import Entidades.models as model
 
 # Configurações do JWT
 SECRET_KEY = "sua-chave-secreta"
@@ -16,23 +20,28 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 # Usuário fake
-fake_users_db = {
-    "FiapUser": {
-        "username": "FiapUser",
-        "hashed_password": pwd_context.hash("FIAP123"),
-    }
-}
+"""
+    username: FiapUser
+    hashed_password: FIAP123
+"""
 
 # Verificar senha
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
 # Função para autenticar o usuário
 def authenticate_user(username: str, password: str):
-    user = fake_users_db.get(username)
-    if not user:
+    user = service.autenticar_usuario(username, password)
+
+    #db_connector.DatabaseManager.read_to_dataframe
+    #db_connector.
+    #user = fake_users_db.get(username)
+    if user.empty:
         return False
-    if not verify_password(password, user["hashed_password"]):
+    if not verify_password(password, user.iloc[0]["Hashed_password"]):
         return False
     return user
 
@@ -59,3 +68,15 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Token inválido ou expirado",
         )
+def cadastro(username: str, password: str) -> bool:
+    data_user ={
+        "User": [username],
+        "Hashed_password": [hash_password(password)]
+    }
+    data_user = pd.DataFrame(data_user)
+    db_manager = DatabaseManager()
+    filters = [(model.Access.User == username)]
+    if not db_manager.read_to_dataframe(model.Access, filters).empty:
+        return False
+    db_manager.insert_from_dataframe(model.Access, data_user)
+    return True
