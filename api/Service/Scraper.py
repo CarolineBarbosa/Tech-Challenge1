db036603c0bd2859706c8e0c7b36a3d937455e86
carol <caroline.c.barbosa@accenture.com>
2025-01-15 21:45:36 -0300

    changing request location

--- api/Service/Scraper.py
+++ api/Service/Scraper.py
@@ -19,26 +19,35 @@ def processa_request(url, max_tentativa_acesso = 2000):
       i = max_tentativa_acesso + 1
     except:
     #   print("Erro listar_subsegmento")
-      i += 1
+        i += 1
+        return None
+
   return data
 
 
 def get_scraper(opcao: str, ano: Optional[str] = None ,subopt: Optional[str] = 1, subop: Optional[str] = None):
-    if ano is None:
-        df_full = []
-        for year in reversed(range(1970, 2024)):
-            df_full.append(scraper_dados(opcao, year, subopt, subop))
-        return df_full
+    url = "" #f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao=opt_{opcao}&subopcao=subopt_{subopt}"
+    data = processa_request(url)
+    print(data)
+    if data is None:        # se o request falhar em todas as tentativas, resgatamos os dados do backup
+       print("AAA")
     else:
-        return scraper_dados(opcao, ano, subopt, subop)
+        if ano is None:
+            df_full = []
+            for year in reversed(range(1970, 2024)):
+                df_full.append(scraper_dados(data, opcao, year, subopt, subop))
+            return df_full
+        else:
+            return scraper_dados(opcao, ano, subopt, subop)
 
 
 #Faz o scraping dos dados de produção, processamento e comercialização do site da Embrapa.
-def scraper_dados(opcao: str, ano: Optional[str] = None ,subopt: Optional[str] = 1, subop: Optional[str] = None):
+def scraper_dados(data, opcao: str, ano: Optional[str] = None ,subopt: Optional[str] = 1, subop: Optional[str] = None):
     
-    url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao=opt_{opcao}&subopcao=subopt_{subopt}"
+    # url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao=opt_{opcao}&subopcao=subopt_{subopt}"
     
-    soup = BeautifulSoup(processa_request(url), "html.parser")
+    # soup = BeautifulSoup(processa_request(url), "html.parser")
+    soup = BeautifulSoup(data, "html.parser")
     table = table = soup.find("table", {"class": "tb_base tb_dados"})  # Localiza a tabela desejada
     data = []
     
