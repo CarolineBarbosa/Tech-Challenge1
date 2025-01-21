4b9e9d91c92d28d0fece817e4469c13a8cb0ea95
Bruno de Oliveira <bruno.olv.feitosa@gmail.com>
2025-01-20 22:06:39 -0300

    change due to python version problems

--- api/utils/auth_util.py
+++ api/utils/auth_util.py
@@ -46,7 +46,7 @@ def authenticate_user(username: str, password: str):
     return user
 
 # Função para criar o token JWT
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
+def create_access_token(data: dict, expires_delta=None):
     to_encode = data.copy()
     expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
