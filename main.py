5e306d41ad3b056e9bc7a79d2ad72a0b7c988307
Bruno de Oliveira <bruno.olv.feitosa@gmail.com>
2025-01-20 22:27:16 -0300

    adiciona rota favicon e inits

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from api.utils import auth_util
 
 app = FastAPI()
 
+# Rota Favicon para evitar 404 por /favicon.ico
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return {"message": "No favicon"}
+
 # Rotas de produção
 app.include_router(controller.router)
 
